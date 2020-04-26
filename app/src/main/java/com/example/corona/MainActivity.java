3958f07ea53bc09eb17f28ac752d9b9a79bfd918
RGBskill <splayer97@hotmail.com>
2020-04-27 00:40:09 +0200

    fixed: reset score

--- app/src/main/java/com/example/corona/MainActivity.java
+++ app/src/main/java/com/example/corona/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragenActivity.punkte = 0;
+
         playbtn = (Button)findViewById(R.id.playbtn);
         playbtn.setOnClickListener(this::starteQuiz);
     }
